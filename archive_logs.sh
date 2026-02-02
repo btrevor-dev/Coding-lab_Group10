63d142f2c223556acd98d1ffcadba2b1bda90f55
Ange-Liana <l.batsinde@alustudent.com>
2026-02-02 03:46:33 -0800

    Validating user input

--- archive_logs.sh
+++ archive_logs.sh
@@ -4,3 +4,8 @@ echo "1.Heart rate"
 echo "2.Temperature "
 echo "3.Water usage"
 read -p "Enter choice (1-3):" choice
+# validating user input
+if [[ ! $choice =~ ^[1-3]$ ]]; then 
+    echo "Invalid selection. Please enter 1, 2, or 3."
+    exit 1
+fi
